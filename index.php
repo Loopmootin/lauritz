0d81cbdc8ec642f3cab054394273e5a7fa1553e9
Loopmootin <christoffera@outlook.dk>
2018-10-16 03:54:39 +0200

    downloading bootstrap locally and playing around with tiles

--- index.php
+++ index.php
@@ -3,12 +3,82 @@
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Lauritz.com</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+
+        <title>Lauritz.com</title>
+
+        <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.min.js"></script>
         <script src="main.js"></script>
     </head>
     <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#"><img src="images/logo.png" alt="lauritz.com logo"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+            </nav>
+        </header>
+
+        <article>
+            <div class="l-width l-height" style="background-image: url('images/lamps.jpg');">
+                <h2>Elektronik</h2>
+            </div>
+            <div class="s-width l-height" style="background-image: url('images/glass.jpg');">
+                <h2>Glas, porcelæn og keramik</h2>
+            </div>
+            <div class="s-width l-height" style="">
+                <h2>Hobby og samleobjekter</h2>
+            </div>
+
+            <div class="s-width" style="">
+                <h2>Jagt, fiskeri, våben og militaria</h2>
+            </div>
+            <div class="s-width" style="">
+                <h2>Lamper og belysning</h2>
+            </div>
+            <div class="s-width" style="">
+                <h2>Maleri og skulpturer</h2>
+            </div>
+            <div class="s-width" style="">
+                <h2>Møbler</h2>
+            </div>
+
+            <div style="">
+                <h2>Rejser og oplevelser</h2>
+            </div>
+            <div style="">
+                <h2>Smykker</h2>
+            </div>
+            <div style="">
+                <h2>Sport og fritid</h2>
+            </div>
+            <div style="">
+                <h2>Sølv, bronze, kobber og tin</h2>
+            </div>
+        </article>
+        <footer>
         
+        </footer>
     </body>
 </html>
