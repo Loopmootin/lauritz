d8a61b0365998e58a0bd139dc0978060bc672edd
Loopmootin <christoffera@outlook.dk>
2018-10-16 14:27:14 +0200

    adding structure to the code, a slider and a few images(only placeholders)

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Lauritz.com</title>
+        <link rel="shortcut icon" type="images/x-icon" href="images/icon.jpg" />
 
         <link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css" />
         <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
@@ -17,66 +18,15 @@
     </head>
     <body>
         <header>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#"><img src="images/logo.png" alt="lauritz.com logo"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-            </nav>
+            <?php include('nav.php') ?>
         </header>
 
+        <?php include('carousel.php') ?>
+
         <article>
-            <div class="l-width l-height" style="background-image: url('images/lamps.jpg');">
-                <h2>Elektronik</h2>
-            </div>
-            <div class="s-width l-height" style="background-image: url('images/glass.jpg');">
-                <h2>Glas, porcelæn og keramik</h2>
-            </div>
-            <div class="s-width l-height" style="">
-                <h2>Hobby og samleobjekter</h2>
-            </div>
-
-            <div class="s-width" style="">
-                <h2>Jagt, fiskeri, våben og militaria</h2>
-            </div>
-            <div class="s-width" style="">
-                <h2>Lamper og belysning</h2>
-            </div>
-            <div class="s-width" style="">
-                <h2>Maleri og skulpturer</h2>
-            </div>
-            <div class="s-width" style="">
-                <h2>Møbler</h2>
-            </div>
-
-            <div style="">
-                <h2>Rejser og oplevelser</h2>
-            </div>
-            <div style="">
-                <h2>Smykker</h2>
-            </div>
-            <div style="">
-                <h2>Sport og fritid</h2>
-            </div>
-            <div style="">
-                <h2>Sølv, bronze, kobber og tin</h2>
-            </div>
+            <?php include('tiles.php') ?>
         </article>
+        
         <footer>
         
         </footer>
---
+++ tiles.php
@@ -0,0 +1,28 @@
+
+
+<div class="l-width l-height" style="background-image: url('images/lamps.jpg');">
+    <h2>Elektronik</h2>
+</div>
+<div class="s-width l-height" style="background-image: url('images/glass.jpg');">
+    <h2>Glas, porcelæn og keramik</h2>
+</div>
+
+<div class="s-width s-height" style="background-image: url('images/ww2.jpg');">
+    <h2>Jagt, fiskeri, våben og militaria</h2>
+</div>
+<div class="s-width s-height" style="background-image: url('images/lamps.jpg');">
+    <h2>Lamper og belysning</h2>
+</div>
+<div class="s-width s-height" style="background-image: url('images/sculpture.jpg');">
+    <h2>Maleri og skulpturer</h2>
+</div>
+
+<div class="s-width s-height" style="background-image: url('images/travel.jpg');">
+    <h2>Rejser og oplevelser</h2>
+</div>
+<div class="s-width s-height" style="background-image: url('images/jewel.jpg');">
+    <h2>Smykker</h2>
+</div>
+<div class="s-width s-height" style="background-image: url('images/ball.jpg');">
+    <h2>Sport og fritid</h2>
+</div>
