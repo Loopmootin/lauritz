ea91051ffe850fa16c43b3460576ac4b90fd4783
Loopmootin <christoffera@outlook.dk>
2018-10-24 13:50:30 +0200

    Nav bar style and functions

--- nav.php
+++ nav.php
@@ -7,42 +7,51 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item dropdown">
-              <li class="nav-item">
-                  <a class="nav-link" href="#">Actions<span class="sr-only"></span></a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="#">Last Chance<span class="sr-only"></span></a>
-              </li>
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Categories
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Electronics</a>
-                    <a class="dropdown-item" href="#">Jewels</a>
-                    <a class="dropdown-item" href="#">Furniture</a>
-                    <a class="dropdown-item" href="#">Clothing</a>
-
-                </div>
+                <li class="nav-item">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownAuctions" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Auctions
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownAuctions">
+                        <a class="dropdown-item" href="#">Auctions</a>
+                        <a class="dropdown-item" href="#">Last Chance</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownCategories" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Categories
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownCategories">
+                        <a class="dropdown-item" href="#">Electronics</a>
+                        <a class="dropdown-item" href="#">Jewels</a>
+                        <a class="dropdown-item" href="#">Furniture</a>
+                        <a class="dropdown-item" href="#">Clothing</a>
+                    </div>
+                </li>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Contact<span class="sr-only"></span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">| Support<span class="sr-only"></span></a>
+                <a class="nav-link" href="#">Support<span class="sr-only"></span></a>
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
             <div class="rightside-nav">
-                <a class="nav-link" href="#"> Login<span class="sr-only"></span></a>
-                <a class="nav-link" href="#"> New user<span class="sr-only"></span></a>
-                <a class="nav-link" href="#"> My page<span class="sr-only"></span></a>
-                <a class="nav-link" href="#"> Sell<span class="sr-only"></span></a>
+                <li class="nav-item">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownUser" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Users
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownUser">
+                        <a class="dropdown-item" href="#">Login</a>
+                        <a class="dropdown-item" href="#">New user</a>
+                        <a class="dropdown-item" href="#">My page</a>
+                        <a class="dropdown-item" href="#">Sell</a>
+                    </div>
+                </li>
             </div>
-        </form>
-        <div class="full-width-div">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
             <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Advanced Search</button>
-        </div>
+        </form>
     </div>
 </nav>
