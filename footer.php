ea35048ef2ca32bb15e714d4cd9eb506001377ec
Unknown <slepzs@gmail.com>
2018-10-24 12:10:27 +0200

    Footer changes

--- footer.php
+++ footer.php
@@ -3,6 +3,10 @@
     <!-- Footer Elements -->
     <div class="container">
 
+
+
+
+
       <!-- Social buttons -->
       <ul class="list-unstyled list-inline text-center">
         <li class="list-inline-item">
@@ -13,11 +17,6 @@
         <li class="list-inline-item">
           <a class="btn-floating btn-tw mx-1">
             <i class="fa fa-twitter"><img src="images/twitter.png" alt=""></i>
-          </a>
-        </li>
-        <li class="list-inline-item">
-          <a class="btn-floating btn-gplus mx-1">
-            <i class="fa fa-google-plus"><img src="images/gplus.png" alt=""></i>
           </a>
         </li>
         <li class="list-inline-item">
@@ -33,7 +32,8 @@
 
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3">
-      <p>Disclaimer: School Project</p>
+      <p>Use of this website implies full acceptance of the conditions of purchase and sale as well as the rules and regulations concerning personal data policy.
+        Copyright Lauritz.com 2004</p>
     </div>
     <!-- Copyright -->
 
