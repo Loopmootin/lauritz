80e6d3bc601d9a4db78b6d5d36654b2a8a2f66c4
Unknown <slepzs@gmail.com>
2018-10-24 14:19:51 +0200

    Footer

--- footer.php
+++ footer.php
@@ -1,14 +1,51 @@
 <footer>
   <div class="container">
+    <div class="footerflex">
   <div class="footer-box-one">
-
+    <p>Change Language</p>
+    <ul>
+      <li><a href="#">Dansk</a></li>
+      <li><a href="#">English</a></li>
+      <li><a href="#">Svenska</a></li>
+      <li><a href="#">Deutsch</a></li>
+      <li><a href="#">Norsk</a></li>
+      <hr>
+      <li><a href="#">Sitemap</a></li>
+      <li><a href="#">Personal Data policy</a></li>
+    </ul>
   </div>
-1  <div class="Footer-box-two">
-
+ <div class="Footer-box-two">
+   <p>Tools</p>
+   <ul>
+     <li><a href="#">Useful Information</a></li>
+     <li><a href="#">Free Online Valuation</a></li>
+     <li><a href="#">Investor Relations Site</a></li>
+     <li><a href="#">Theme of the month: Toy</a></li>
+     <li><a href="#">Theme Auctions</a></li>
+     <hr>
+     <li><a href="#">Highlights</a></li>
+     <li><a href="#">deLuxe</a></li>
+   </ul>
   </div>
   <div class="Footer-box-three">
-
+    <p>Categories</p>
+    <ul>
+      <li><a href="#">Carpets, Rugs, Textiles</a></li>
+      <li><a href="#">Clothing, shoes and accessories</a></li>
+      <li><a href="#">Electronics</a></li>
+      <li><a href="#">Funiture</a></li>
+      <li><a href="#">Hobby and collectibles</a></li>
+      <hr>
+      <li><a href="#">Home and garden</a></li>
+      <li><a href="#">Sport and leisure</a></li>
+    </ul>
+  </div>
   </div>
 </div>
-
+<!-- Copyright -->
+   <div class="footer-copyright text-center py-3">
+     <p>Use of this website implies full acceptance of the conditions of purchase and sale as well as the rules and regulations concerning personal data policy.
+       Copyright Lauritz.com 2004</p>
+   </div>
+   <!-- Copyright -->
 </footer>
